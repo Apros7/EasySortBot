10e0ce13a999cbb6b482cb1c6b142359db4b93de
apros7 <lucas.vilsen@gmail.com>
2024-04-06 17:14:10 +0200

    accellstepper heart

--- control/AccellStepper/AccellStepper.ino
+++ control/AccellStepper/AccellStepper.ino
@@ -4,13 +4,21 @@
 
 // Define the stepper motor and the pins that is connected to
 AccelStepper stepper1(1, 2, 5); // (Typeof driver: with 2 pins, STEP, DIR)
-AccelStepper stepper2(1, 3, 6);
-AccelStepper stepper3(1, 4, 7);
-// AccelStepper stepper4(1, 12, 13);
+// AccelStepper stepper2(1, 3, 6);
+AccelStepper stepper2(1, 4, 7);
+AccelStepper stepper3(1, 12, 13);
 
 MultiStepper steppersControl;  // Create instance of MultiStepper
 
 long gotoposition[3]; // An array to store the target positions for each stepper motor
+
+void run(int x, int y) {
+  gotoposition[0] = x;
+  gotoposition[1] = x;
+  gotoposition[2] = y;
+  steppersControl.moveTo(gotoposition); // Calculates the required speed for all motors
+  steppersControl.runSpeedToPosition(); // Blocks until all steppers are in position
+}
 
 void setup() {
 
@@ -22,48 +30,20 @@ void setup() {
   steppersControl.addStepper(stepper1);
   steppersControl.addStepper(stepper2);
   steppersControl.addStepper(stepper3);
-}
 
-// void mmToSteps(int amount) {
-//   // 1 teeth is 2 mm
-//   // 1 round is 20 teeth
-//   // 200 steps is one round
-//   // each step is 40/200 mm
-//   return amount * 200/40
-// }
-
-// void moveX(int mm) {
-//   int steps = mmToSteps(mm)
-//   gotoposition[0] = steps;
-//   gotoposition[1] = steps;
-// }
-
-// void moveY() {
-//   int steps = mmToSteps(mm)
-//   gotoposition[2] = steps;
-// }
-
-void run() {
-  steppersControl.moveTo(gotoposition); // Calculates the required speed for all motors
-  steppersControl.runSpeedToPosition(); // Blocks until all steppers are in position
+  run(0, 0);
+  delay(1000);
+  run(1200, -1200);
+  run(1800, -1200);
+  run(2100, -900);
+  run(2100, -600);
+  run(1500, 0);
+  run(2100, 600);
+  run(2100, 900);
+  run(1800, 1200);
+  run(1200, 1200);
+  run(0, 0);
 }
 
 void loop() {
-  // Store the target positions in the "gotopostion" array
-  gotoposition[0] = 1000;
-  gotoposition[1] = 1000;
-  gotoposition[2] = 1000;
-
-  run();
-
-  delay(1000);
-
-  gotoposition[0] = 0;
-  gotoposition[1] = 0;
-  gotoposition[2] = 0;
-
-  steppersControl.moveTo(gotoposition);
-  steppersControl.runSpeedToPosition();
-
-  delay(1000);
 }
